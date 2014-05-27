adb9465b19664bc4597fb2a8594c7317cfce7426
Maciej Kwiek <maciej.kwiek@entertailor.com>
2014-05-27 12:37:38 +0200

    hot_redis/core/tests/tests.py

--- hot_redis/core/tests/tests.py
+++ hot_redis/core/tests/tests.py
@@ -5,7 +5,7 @@ import Queue
 import unittest
 from hot_redis.utils import make_key
 from hot_redis.core.tests.test_utils import get_redis_connection
-from hot_redis.contrib.django.utils import DEFAULT_TEST_PREFIX
+from hot_redis.utils import DEFAULT_TEST_PREFIX
 from hot_redis.utils import delete_by_pattern
 from hot_redis import core, ObjectList
 
