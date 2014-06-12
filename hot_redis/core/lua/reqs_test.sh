905e3f00e88204446d3acad05af5b8ab535fd5fc
Maciej Kwiek <maciej.kwiek@entertailor.com>
2014-06-12 09:59:20 +0200

    Moved lua test requirement install to separate file

---
+++ hot_redis/core/lua/reqs_test.sh
@@ -0,0 +1,4 @@
+sudo luarocks install redis-lua
+sudo luarocks install inspect
+sudo luarocks install lunatest
+sudo luarocks install underscore.lua --from=http://marcusirven.s3.amazonaws.com/rocks/
--- hot_redis/core/lua/test.sh
+++ hot_redis/core/lua/test.sh
@@ -1,5 +1,1 @@
-sudo luarocks install redis-lua
-sudo luarocks install inspect
-sudo luarocks install lunatest
-sudo luarocks install underscore.lua --from=http://marcusirven.s3.amazonaws.com/rocks/
 lua tests.lua
