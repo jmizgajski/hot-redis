7bd767e180de750c14c95e2b453867d93885a59e
Maciej Kwiek <maciej.kwiek@entertailor.com>
2014-06-23 15:53:07 +0200

    Added django version of prefix_key

--- hot_redis/contrib/django/utils.py
+++ hot_redis/contrib/django/utils.py
@@ -1,16 +1,17 @@
 # -*- coding: utf8 -*-
 from django.conf import settings
-
-DEFAULT_TEST_PREFIX = '__test__'
-KEY_SEPARATOR = '.'
-
+from hot_redis.utils import KEY_SEPARATOR, DEFAULT_TEST_PREFIX
 
 def make_key(*parts):
     key = KEY_SEPARATOR.join(str(part) for part in parts)
 
+    return key
+
+
+def prefix_key(key):
     if getattr(settings, 'REDIS_TEST', False):
         prefix = getattr(
             settings, 'REDIS_TEST_KEY_PREFIX', DEFAULT_TEST_PREFIX)
-        return "%s%s" % (prefix, key)
+        return "%s%s%s" % (prefix, KEY_SEPARATOR,  key)
 
     return key
