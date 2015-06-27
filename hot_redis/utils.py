9a35a9bd5ce8f14c75abad35e76bc1345da11098
jmizgajski <mizgajski.jan@gmail.com>
2015-06-27 15:04:54 +0200

    Added expire by pattern utility method

--- hot_redis/utils.py
+++ hot_redis/utils.py
@@ -35,6 +35,27 @@ def delete_by_pattern(pattern, client):
     client.eval(delete_script, 0, pattern)
 
 
+def expire_by_pattern(pattern, ttl, client):
+    """
+    This is a turbo efficient LUA script that will atomically delete all
+    keys that match a pattern and never run out of space
+
+    :param pattern: a redis key pattern i.e. "your_prefix:*"
+    :type pattern: str
+    :param ttl: time to live that should be set for keys matched by pattern
+    in second
+    :type ttl: int
+    :param client: redis client on which you would like to do this operation
+    :type client: Redis
+    """
+    ttl_script = '''
+    for _,k in ipairs(redis.call('keys', ARGV[1])) do
+        redis.call('expire', k, ARGV[2])
+    end
+    '''
+    client.eval(ttl_script, 0, pattern, ttl)
+
+
 DEFAULT_TEST_PREFIX = '__test__'
 KEY_SEPARATOR = '.'
 
